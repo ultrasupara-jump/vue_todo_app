933b6f7ff1b7894420c91e5a525483973d1905b9
ultrasupara-jump <atsu120225@gmail.com>
2022-04-26 22:48:57 +0900

    アプリ1段階

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
-import { createApp } from 'vue'
+import { createApp, VueElement } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { initializeApp } from 'firebase/app'; //追加
+
+VueElement.config.productionTip = false
 
 createApp(App).use(router).mount('#app')
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBuGaLC4kkOHp0dEd2V_eepTZ_3lzZdwMU",
+    authDomain: "vue-todo-app-92f9f.firebaseapp.com",
+    projectId: "vue-todo-app-92f9f",
+    storageBucket: "vue-todo-app-92f9f.appspot.com",
+    messagingSenderId: "267300502849",
+    appId: "1:267300502849:web:a6c7e32826158f66b275c0"
+};
+
+initializeApp(firebaseConfig); //追加
